85ffffe36b6eda2d07114ce1331972bec9376c01
Ellie <elliewawrzaszek@gmail.com>
2017-08-22 09:49:23 -0400

    Add sessions and application controller auth methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,36 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  private
+
+  def current_user
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login(user)
+    @current_user = user
+    user.reset_token!
+    session[:session_token] = user.session_token
+  end
+
+  def logout
+    current_user.try(:reset_token!)
+    session[:session_token] = nil
+    @current_user = nil
+  end
+
+  def require_logged_in
+    unless current_user
+      render json: { base: ['invalid credentials'] }, status: 401
+    end
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
 
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:create]
+    resource :session, only: [:create, :destroy, :show]
+  end
+
   root "static_pages#root"
 end
