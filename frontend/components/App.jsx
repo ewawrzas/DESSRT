ba2c4ba94b790d74831896ec7ef9ae75d034e030
Ellie <elliewawrzaszek@gmail.com>
2017-08-22 15:25:14 -0400

    Add auth route paths

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import GreetingContainer from './greeting/greeting_container'
-import AuthFormContainer from './authform/auth_form_container'
-import { Route } from 'react-router-dom'
+import React from 'react';
+import GreetingContainer from './greeting/greeting_container';
+import AuthFormContainer from './authform/auth_form_container';
+import { Route } from 'react-router-dom';
+import { AuthRoute } from '../util/route_util';
 
 const App = () => (
   <div>
@@ -10,8 +11,8 @@ const App = () => (
       <GreetingContainer/>
     </header>
 
-    <Route path="/login" component={AuthFormContainer} />
-    <Route path="/signup" component={AuthFormContainer} />
+    <AuthRoute path="/login" component={AuthFormContainer} />
+    <AuthRoute path="/signup" component={AuthFormContainer} />
   </div>
 );
 
