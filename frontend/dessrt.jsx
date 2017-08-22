4b4e233f9b53b93cdd6fc1bdaa64665b897b0ebb
Ellie <elliewawrzaszek@gmail.com>
2017-08-22 11:54:56 -0400

    Add App and root components

--- frontend/dessrt.jsx
+++ frontend/dessrt.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { login, logout, signup } from './util/session_api_util'
+import Root from './components/root';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch
+
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome :)</h1>, root);
+  ReactDOM.render(<Root store={store}/>, root);
 });
-
-
-window.login = login
-window.logout = logout
-window.signup = signup
