0eb1390ac24322a73bc3060adebb17fbbfd05be2
Ellie <elliewawrzaszek@gmail.com>
2017-08-29 15:51:34 -0400

    Add links to feed

--- frontend/components/checkins/checkinItem.jsx
+++ frontend/components/checkins/checkinItem.jsx
@@ -3,10 +3,13 @@ import { Link } from 'react-router-dom';
 
 
 const CheckinItem = ({ checkin }) => {
-  const starRating = `rating${checkin.rating}`
+  const starRating = `rating${checkin.rating}`;
+  const userLink = <Link to={`/users/${checkin.user_id}`}><span id="feedUserLink">{checkin.user}</span></Link>;
+  const dessertLink = <Link to={`/desserts/${checkin.dessert_id}`}><span id="feedUserLink">{checkin.dessert}</span></Link>;
+debugger
   return (
   <div className="checkinItem">
-    <span id="checkinTitle">{`${checkin.user} is eating ${checkin.dessert}`}</span>
+  <span id="checkinTitle">{userLink} is eating {dessertLink}</span>
     <div className="checkinComment">
       <span>{checkin.comment}</span>
       <span id={starRating}>Rating</span>
