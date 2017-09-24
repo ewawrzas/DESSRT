beac2034e3d9d2a46f8ce3ba4b010048289dcbb6
Ellie <elliewawrzaszek@gmail.com>
2017-09-24 18:50:11 -0400

    Add background color to body

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -6,9 +6,9 @@ const Footer = () => {
     <div className="footer">
       <ul className="footerLinks">
   			<li><a href="ellewawrzaszek.us" className="icon fa-linkedin"><span className="label">©Ellie Wawrzaszek 2017</span></a></li>
-  			<li><a href="https://www.linkedin.com/in/ellie-wawrzaszek/" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
-  			<li><a href="https://github.com/ewawrzas" className="icon fa-github"><span className="label">Github</span></a></li>
-  			<li><a href="https://angel.co/ellie-wawrzaszek" className="icon fa-angellist"><span className="label">AngelList</span></a></li>
+  			<li><a href="http://www.linkedin.com/in/ellie-wawrzaszek/" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
+  			<li><a href="http://github.com/ewawrzas" className="icon fa-github"><span className="label">Github</span></a></li>
+  			<li><a href="http://angel.co/ellie-wawrzaszek" className="icon fa-angellist"><span className="label">AngelList</span></a></li>
   		</ul>
     </div>
   )
