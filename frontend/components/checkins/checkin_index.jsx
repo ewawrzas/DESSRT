f12c43850f516f11ee1227d1830197265a84c9f6
Ellie <elliewawrzaszek@gmail.com>
2017-09-24 11:41:09 -0400

    Refactor checkin index and home page

--- frontend/components/checkins/checkin_index.jsx
+++ frontend/components/checkins/checkin_index.jsx
@@ -17,17 +17,12 @@ class CheckinIndex extends React.Component {
 
     const { checkins, deleteCheckin, currentUser, fetchAllCheckins } = this.props
 
-
     return (
-      <div>
-
-          <div className="checkinsIdx">
-              <div className="checkinList">
-                {checkins.map(checkin => <CheckinItem key={checkin.id} checkin={checkin} deleteCheckin={deleteCheckin} currentUser={currentUser} fetchAllCheckins={fetchAllCheckins} />)}
-                <h2 id="feed">Recent Activity</h2>
-              </div>
-            </div>
-
+      <div className="checkinsIdx">
+        <div className="checkinList">
+          {checkins.map(checkin => <CheckinItem key={checkin.id} checkin={checkin} deleteCheckin={deleteCheckin} currentUser={currentUser} fetchAllCheckins={fetchAllCheckins} />)}
+          <h2 id="feed">Recent Activity</h2>
+        </div>
       </div>
 
     );
--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -1,58 +1,25 @@
 import React, { Component } from 'react';
 import { Route, NavLink, Link } from 'react-router-dom';
 
-import UserItem from '../profile/user_item'
-import DessertItem from '../desserts/dessert_item'
-import UpdateFormContainer from '../profile/update_form_container';
-import UserProfileContainer from '../profile/user_profile_container';
-import DessertIndexContainer from '../desserts/dessert_index_container';
 import CheckinIndexContainer from '../checkins/checkin_index_container';
 import NavContainer from '../nav/nav_container';
 
 class Greeting extends React.Component {
   constructor(props) {
     super(props);
-    // this.handleClick = this.handleClick.bind(this);
-    // this.handleKeyPress = this.handleKeyPress.bind(this);
-    // this.handleChange = this.handleChange.bind(this);
-    // this.state = {
-    //   search: ""
-    // }
   }
 
-  // handleClick() {
-  //   this.props.logout();
-  // }
-  //
-  // handleChange(field) {
-  //   return (e) => this.setState({ [field]: e.currentTarget.value });
-  // }
-  //
-  // handleKeyPress(e) {
-  //   if (e.key === 'Enter') {
-  //   this.props.fetchAllDesserts(this.state).then(
-  //     () => this.setState({
-  //     search: ""
-  //     })
-  //   ).then(search => this.props.history.push(`/search`));
-  //   }
-  // }
 
   render () {
     return (
       <div className="greeting">
         <NavContainer />
-
         <div className="homePage">
-
           <div className="homeContent">
             <p id="user">{this.props.currentUser.username}</p>
           </div>
           <CheckinIndexContainer />
-          <Route path ="/users/:userId" component={UserProfileContainer} />
-          <Route path="/users/:userId/update" component={UpdateFormContainer} />
         </div>
-
       </div>
     );
   }
