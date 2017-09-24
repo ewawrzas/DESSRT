4e4e36d85c82790ea6e21c7b3197d278c132631d
Ellie <elliewawrzaszek@gmail.com>
2017-09-24 16:03:40 -0400

    Format footer

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -5,6 +5,7 @@ const Footer = () => {
   return (
     <div className="footer">
       <ul className="footerLinks">
+  			<li><a href="ellewawrzaszek.us" className="icon fa-linkedin"><span className="label">©Ellie Wawrzaszek 2017</span></a></li>
   			<li><a href="https://www.linkedin.com/in/ellie-wawrzaszek/" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
   			<li><a href="https://github.com/ewawrzas" className="icon fa-github"><span className="label">Github</span></a></li>
   			<li><a href="https://angel.co/ellie-wawrzaszek" className="icon fa-angellist"><span className="label">AngelList</span></a></li>
